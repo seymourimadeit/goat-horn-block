977b8d343a2f130783907a390704c6f6a9087e21
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2023-01-16 20:15:26 -0800

    port to 1.19.3

--- src/main/java/tallestred/goathornblock/GoatHornBlockMod.java
+++ src/main/java/tallestred/goathornblock/GoatHornBlockMod.java
@@ -1,15 +1,18 @@
 package tallestred.goathornblock;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,26 +24,20 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+import tallestred.goathornblock.common.blocks.GoatHornBlock;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GoatHornBlockMod.MODID)
 public class GoatHornBlockMod {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "goat_horn_block_mod";
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("goat_horn_amplifier", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("goat_horn_amplifier", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    // Directly reference a slf4j logger
+    public static final RegistryObject<Block> GOAT_HORN = BLOCKS.register("goat_horn_amplifier", () -> new GoatHornBlock(BlockBehaviour.Properties.of(Material.STONE)));
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public GoatHornBlockMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
         BLOCKS.register(modEventBus);
-        ITEMS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -50,6 +47,19 @@ public class GoatHornBlockMod {
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
+    }
+
+
+    @SubscribeEvent
+    public void onItemRightClick(PlayerInteractEvent.RightClickBlock event) {
+        ItemStack playerItemStack = event.getItemStack();
+        Player player = event.getEntity();
+        if (playerItemStack.getItem() == Items.GOAT_HORN) {
+            player.swing(InteractionHand.MAIN_HAND);
+            BlockPos pos = event.getHitVec().getBlockPos().relative(event.getHitVec().getDirection());
+            player.getLevel().setBlock(pos, GoatHornBlockMod.GOAT_HORN.get().defaultBlockState(), 11);
+            player.getLevel().gameEvent(GameEvent.BLOCK_PLACE, pos, GameEvent.Context.of(player, GoatHornBlockMod.GOAT_HORN.get().defaultBlockState()));
+        }
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
--- src/main/java/tallestred/goathornblock/common/blocks/GoatHornBlock.java
+++ src/main/java/tallestred/goathornblock/common/blocks/GoatHornBlock.java
@@ -1,9 +1,22 @@
 package tallestred.goathornblock.common.blocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GoatHornBlock extends Block {
+    protected static final VoxelShape SHAPE = Shapes.or(Block.box(4, -1, 0, 12, 7, 11), Block.box(6, 0, 11, 10, 4, 17));
+
     public GoatHornBlock(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
     }
 }
